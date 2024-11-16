29697c64b82be7c4c3623c26d30962e7f18fd167
blackstraysheep <106660155+blackstraysheep@users.noreply.github.com>
2024-11-17 04:13:35 +0900

    Add files via upload

---
+++ team.js
@@ -0,0 +1,11 @@
+//teamname1からteamname6に，haiku.jsで学校ごとに指定した変数を割り当てることで、組み合わせを一括で変更することができます．
+var teamname1 = yuniba
+var teamname2 = nadaC
+var teamname3 = ""
+var teamname4 = ""
+var teamname5 = ""
+var teamname6 = ""
+
+var teamnamefin1 = ""
+var teamnamefin2 = ""
+

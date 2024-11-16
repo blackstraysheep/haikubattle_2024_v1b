65829a8697057a4b28c6e02575e48338808f3303
blackstraysheep <106660155+blackstraysheep@users.noreply.github.com>
2024-11-17 04:15:01 +0900

    Add files via upload

---
+++ A1/teamname.js
@@ -0,0 +1,12 @@
+const redteam = document.getElementById('redteam');
+redteam.textContent =  teamname1[0][0];
+
+const whiteteam = document.getElementById('whiteteam');
+whiteteam.textContent = teamname2[0][0];
+
+const gameid = document.getElementById('gameid');
+//gameid.textContent = "Ａブロック　第１試合";
+gameid.textContent = "決勝戦";
+
+const gamekendai = document.getElementById('thiskendai');
+thiskendai.textContent = "「" + kendai[1] + "」";
